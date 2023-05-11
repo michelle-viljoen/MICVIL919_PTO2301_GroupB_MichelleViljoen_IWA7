3d2a37a925c9762ee7c9f1ab119ae19dd57c04ab
michelle-viljoen <126607925+michelle-viljoen@users.noreply.github.com>
2023-05-11 20:51:50 +0200

    Add files via upload
    Challenge 2 complete

---
+++ Challenge2/script.js
@@ -0,0 +1,9 @@
+const nickname= "";
+const firstname = "Timothy";
+
+//console.log("Good Morning, ${nickname} || {firstname}!")
+
+// console.log('Good morning', nickname + '!' || firstname + '!')
+
+console.log('Good morning',(nickname||firstname),'!')
+
